831244dcdc56e956eca431ba07474ecb4669fdce
Ryan Toler <rdt712@gmail.com>
2021-05-12 00:30:56 -0400

    Added built in support for nested routes and updated mdx remote

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/about', title: 'About' },
   { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
